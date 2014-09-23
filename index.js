c07d35461e495fb12f91e0f1453bcd2a09006d20
Glenn Sonna <kenpachi.os@gmail.com>
2014-09-23 19:27:24 +0200

    Rearchitecturised, routing++

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-var app = require('./lib/app').app;
+/*
+ * 
+ * https://github.com/leoht/tenveux
+ *
+ * Copyright (c) 2014 
+ * Licensed under the MIT license.
+ */
+ 'use strict';
 
-app.listen(3000);
-console.log(' [x] Listening on port 3000');
+ var app = require('./lib/app').app;
+ var port = Number(process.env.PORT || 5000);
+
+ app.listen(port);
+ console.log(' [x] Listening on port ' + port);
