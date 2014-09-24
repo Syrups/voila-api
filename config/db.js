ea5fc6de988ffc3a500872530be47b27ebde69a5
Léo Hetsch <leo.hetsch@gmail.com>
2014-09-24 15:33:00 +0200

    updates

--- config/db.js
+++ config/db.js
@@ -1,5 +1,13 @@
 // CONFIG
 
+// module.exports = {
+// 	url: 'kahana.mongohq.com',
+// 	port: '10067',
+// 	user: 'leo',
+// 	password: 'doliprane',
+// 	name: 'tenveux'
+// };
+
 module.exports = {
 	url: 'localhost',
 	port: '27017',
