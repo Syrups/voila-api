bd59ea8471ed02e95acd3e26f6b6a4ed9f33dfcc
Léo Hetsch <leo.hetsch@gmail.com>
2014-10-10 22:45:07 +0200

    Updates yeah

--- config/twilio.js
+++ config/twilio.js
@@ -1,6 +1,7 @@
 // Twilio config
 
 module.exports = {
-	accountId: '08798798236',
-	authToken: 'sdjlghsdjkdskglh'
+	accountId: 'ACeb48731954707f91eb8481f27f40e5f6',
+	authToken: '2b22817f0593810529a2d3323fd9b909',
+	number: '+33970730606'
 };
