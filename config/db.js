e293d60319d47a7b29b7c6175b44b415fdd050ad
Léo Hetsch <leo.hetsch@gmail.com>
2015-01-22 15:57:47 +0100

    Lot of modifications and unit testing yeah

--- config/db.js
+++ config/db.js
@@ -1,9 +1,17 @@
 // CONFIG
 
+// module.exports = {
+// 	url: 'ds063180.mongolab.com',
+// 	port: '63180',
+// 	name: 'heroku_app32519883',
+// 	user: 'tenveux',
+// 	password: 'jenprends'
+// };
+
 module.exports = {
-	url: 'ds063180.mongolab.com',
-	port: '63180',
-	name: 'heroku_app32519883',
-	user: 'tenveux',
-	password: 'jenprends'
+	url: 'localhost',
+	port: '27017',
+	name: 'tenveux',
+	user: '',
+	password: ''
 };
