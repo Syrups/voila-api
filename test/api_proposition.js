d6fe443b0d06f123c43c54bb7ef4331b3a4058cc
Glenn Sonna <kenpachi.os@gmail.com>
2015-01-24 16:25:59 +0100

    mocha proposition test

---
+++ test/api_proposition.js
@@ -0,0 +1,61 @@
+var app = require('../lib/app').app;
+var request = require('supertest');
+var assert = require('assert');
+var Proposition = require('../lib/models/proposition');
+var User = require('../lib/models/user');
+
+describe('POST /propositions', function () {
+
+	var receivers = ["54383304d3c611e8084ab5f0"];
+
+	var zifID;
+	var fizID;
+	var token;
+
+	var data;
+
+	before(function (done) {
+		request(app)
+			.post('/api/users')
+			.send({
+				username: 'oof',
+				password: 'rab'
+			})
+			.expect(201)
+			.end(function (err, req) {
+				var res = JSON.parse(req.res.text);
+				zifID = res.id;
+				token = res.token;
+
+				done();
+			});
+	});
+
+	it('should create new proposition', function (done) {
+
+		request(app)
+			.post('/api/propositions')
+			.set('X-Authorization-Token', token)
+			.send({
+				receivers: receivers,
+				image: "http://www.online-image-editor.com//styles/2014/images/example_image.png"
+			})
+			.expect(201).end(function (err, req) {
+				res = JSON.parse(req.res.text);
+				assert('data' in res, 'Data is not present in response');
+
+				data = res.data;
+
+				assert('id' in data, 'id is not present in Data');
+
+				Proposition.findByIdAndRemove(data.id, done);
+			});
+	});
+
+
+	after(function (done) {
+
+		User.findByIdAndRemove(zifID, done);
+		//User.findByIdAndRemove(fizID, done);
+	});
+});
--- test/api_users.js
+++ test/api_users.js
@@ -3,87 +3,87 @@ var request = require('supertest');
 var assert = require('assert');
 var User = require('../lib/models/user');
 
-describe('POST /users', function() {
-  it('should return new user data', function(done) {
-   	request(app)
-   		.post('/api/users')
-   		.send({
-   			username: 'foo',
-   			password: 'bar'
-   		})
-   		.expect(201)
-   		.end(function (err, req) {
-   			res = JSON.parse(req.res.text);
-   			assert('id' in res, 'ID is not present');
-   			assert('token' in res, 'Token is not present');
+describe('POST /users', function () {
+  it('should return new user data', function (done) {
+    request(app)
+      .post('/api/users')
+      .send({
+        username: 'foo',
+        password: 'bar'
+      })
+      .expect(201)
+      .end(function (err, req) {
+        res = JSON.parse(req.res.text);
+        assert('id' in res, 'ID is not present');
+        assert('token' in res, 'Token is not present');
 
-   			User.findByIdAndRemove(res.id, done);
-   		});
+        User.findByIdAndRemove(res.id, done);
+      });
   });
 });
 
-describe('GET /users/authenticate', function() {
+describe('GET /users/authenticate', function () {
   var id, token;
 
   before(function (done) {
-  	request(app)
-   		.post('/api/users')
-   		.send({
-   			username: 'foo',
-   			password: 'bar'
-   		})
-   		.expect(201)
-   		.end(function (err, req) {
-   			res = JSON.parse(req.res.text);
-   			id = res.id;
-   			token = res.token;
+    request(app)
+      .post('/api/users')
+      .send({
+        username: 'foo',
+        password: 'bar'
+      })
+      .expect(201)
+      .end(function (err, req) {
+        res = JSON.parse(req.res.text);
+        id = res.id;
+        token = res.token;
 
-   			done();
-   		});
+        done();
+      });
   })
 
   it('should authenticate with good credentials', function (done) {
 
-  	request(app)
-  	  .get('/api/users/authenticate')
-  	  .send({
-  	    username: 'foo',
-   		password: 'bar'
-  	  })
-  	  .expect(200)
-  	  .end(function (err, req) {
-  	  	res = JSON.parse(req.res.text);
+    request(app)
+      .get('/api/users/authenticate')
+      .send({
+        username: 'foo',
+        password: 'bar'
+      })
+      .expect(200)
+      .end(function (err, req) {
+        res = JSON.parse(req.res.text);
 
-  	  	assert('token' in res, 'Token is not present in response');
-  	  	assert(res.token == token, 'Token is not the good one');
+        assert('token' in res, 'Token is not present in response');
+        assert(res.token == token, 'Token is not the good one');
 
 
-  	  	done();
-  	  });
+        done();
+      });
   });
 
   it('should throw not found with bad username', function (done) {
-  	request(app)
-  	  .get('/api/users/authenticate')
-  	  .send({
-  	    username: 'foobar',
-   		password: 'bar'
-  	  })
-  	  .expect(404, done);
+    request(app)
+      .get('/api/users/authenticate')
+      .send({
+        username: 'foobar',
+        password: 'bar'
+      })
+      .expect(404, done);
   });
 
   it('should deny access with bad credentials', function (done) {
-  	request(app)
-  	  .get('/api/users/authenticate')
-  	  .send({
-  	    username: 'foo',
-   		password: 'baz'
-  	  })
-  	  .expect(403, done);
+    request(app)
+      .get('/api/users/authenticate')
+      .send({
+        username: 'foo',
+        password: 'baz'
+      })
+      .expect(403, done);
   });
 
   after(function (done) {
-  	User.findByIdAndRemove(id, done);
+    User.findByIdAndRemove(id, done);
   });
 });
 
@@ -91,31 +91,31 @@ describe('GET /users/:id', function () {
   var id, token;
 
   before(function (done) {
-  	request(app)
-   		.post('/api/users')
-   		.send({
-   			username: 'foo',
-   			password: 'bar'
-   		})
-   		.expect(201)
-   		.end(function (err, req) {
-   			res = JSON.parse(req.res.text);
-   			id = res.id;
-   			token = res.token;
+    request(app)
+      .post('/api/users')
+      .send({
+        username: 'foo',
+        password: 'bar'
+      })
+      .expect(201)
+      .end(function (err, req) {
+        res = JSON.parse(req.res.text);
+        id = res.id;
+        token = res.token;
 
-   			done();
-   		});
+        done();
+      });
   });
 
   it('should respond with json data', function (done) {
-  	request(app)
-  		.get('/api/users/'+id)
-  		.set('X-Authorization-Token', token)
-  		.expect('Content-Type', /json/)
-  		.expect(200, done);
+    request(app)
+      .get('/api/users/' + id)
+      .set('X-Authorization-Token', token)
+      .expect('Content-Type', /json/)
+      .expect(200, done);
   });
 
   after(function (done) {
-  	User.findByIdAndRemove(id, done);
+    User.findByIdAndRemove(id, done);
   });
 });
