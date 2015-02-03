0f689255613bc24f0f61b65359804cfd62ad7bf8
Glenn Sonna <kenpachi.os@gmail.com>
2015-02-03 15:17:01 +0100

    fix lot of bugs

--- test/proposition.js
+++ test/proposition.js
@@ -4,107 +4,172 @@ var assert = require('assert');
 var Proposition = require('../lib/models/proposition');
 var User = require('../lib/models/user');
 
+var ObjectId = require('mongoose').Types.ObjectId;
+
 
 var Utils = require('./test_utils');
 
-describe('POST /propositions', function () {
+describe('Prospositions API', function () {
 
-	var receivers = ["54383304d3c611e8084ab5f0"];
-	var reReceivers = ["54383508133b7ed110e5f24f", "5455480fa57c6c000002eedf"];
+	var receivers = [];
+	var reReceivers = [];
 
-	var zifID;
-	var fizID;
-	var token;
+	var glennID, sylID, leoID, oofId, fizID;
+	var token, glennToken, sylToken;
 
 	var data;
 
 	var prodsition1ID, prodsition2ID;
 
 	before(function (done) {
-		Utils.createUser('oof', 'rab', function (res) {
-			zifID = res.id;
+
+		Utils.createUser('oof', 'rab', 'o@gmail.com', function (res) {
+			oofId = res.id;
 			token = res.token;
+		});
+
+		Utils.createUser('glenn', 'google', 'g@gmail.com', function (res) {
+			glennID = res.id;
+			glennToken = res.token;
+
+			receivers.push(glennID);
+
+		});
+
+		Utils.createUser('syl', 'tise', 'st@gmail.com', function (res) {
+			sylID = res.id;
+			sylToken = res.token;
+
+			receivers.push(sylID);
+
+		});
+
+		Utils.createUser('leo', 'ruby', 'leoht@gmail.com', function (res) {
+			leoID = res.id;
+
+			reReceivers.push(leoID);
 
 			done();
 		});
-	});
 
-	it('should create new proposition', function (done) {
-
-
-		request(app)
-			.post('/api/propositions')
-			.set('X-Authorization-Token', token)
-			.send({
-				receivers: receivers,
-				image: "http://www.online-image-editor.com//styles/2014/images/example_image.png"
-			})
-			.expect(201).end(function (err, req) {
-				res = JSON.parse(req.res.text);
-				assert('data' in res, 'Data is not present in response');
-
-				data = res.data;
-
-				assert('_id' in data, 'id is not present in Data');
-
-				prodsition1ID = data.id;
-
-				done();
-				//Proposition.findByIdAndRemove(data.id, done);	
-			})
-	});
-
-	it('should resend proposition with originalProposition param', function (done) {
-
-		request(app)
-			.post('/api/propositions')
-			.set('X-Authorization-Token', token)
-			.send({
-				receivers: reReceivers,
-				originalProposition: prodsition1ID
-			})
-			.expect(201).end(function (err, req) {
-				res = JSON.parse(req.res.text);
-				assert('data' in res, 'Data is not present in response');
-
-				data = res.data;
-
-				assert('_id' in data, 'id is not present in Data');
-
-				prodsition2ID = data.id;
-
-				Proposition.findByIdAndRemove(prodsition1ID);
-				Proposition.findByIdAndRemove(prodsition2ID, done);
-			});
 	});
 
 
-	it('should throw error 403', function (done) {
 
-		request(app)
-			.post('/api/propositions')
-			//.set('X-Authorization-Token', token)
-			.send({
-				user: "ok",
-				receivers: receivers,
-				maninthemiddle: "hahahaha!"
-			})
-			.expect(403).end(function (err, req) {
+	describe('POST /propositions', function () {
+		it('should create new proposition', function (done) {
+			request(app)
+				.post('/api/propositions')
+				.set('X-Authorization-Token', token)
+				.send({
+					receivers: receivers,
+					image: "http://www.online-image-editor.com//styles/2014/images/example_image.png"
+				})
+				.expect(201).end(function (err, req) {
+					var res = req.res.body;
 
-				data = JSON.parse(req.res.text);
+					assert('data' in res, 'Data is not present in response');
 
-				assert('message' in data, 'message is not present in Data');
-				done();
+					var data = res.data;
 
-			});
+					assert('_id' in data, 'id is not present in Data');
+
+					prodsition1ID = data.id;
+
+					done();
+					//Proposition.findByIdAndRemove(data.id, done);	
+				})
+		});
+
+		it('should resend proposition with originalProposition param', function (done) {
+
+			request(app)
+				.post('/api/propositions')
+				.set('X-Authorization-Token', token)
+				.send({
+					receivers: reReceivers,
+					originalProposition: prodsition1ID
+				})
+				.expect(201).end(function (err, req) {
+					res = JSON.parse(req.res.text);
+					assert('data' in res, 'Data is not present in response');
+
+					data = res.data;
+
+					assert('_id' in data, 'id is not present in Data');
+
+					prodsition2ID = data.id;
+
+					done();
+				});
+		});
+
+
+		it('should throw error 403', function (done) {
+
+			request(app)
+				.post('/api/propositions')
+				//.set('X-Authorization-Token', token)
+				.send({
+					user: "ok",
+					receivers: receivers,
+					maninthemiddle: "hahahaha!"
+				})
+				.expect(403).end(function (err, req) {
+
+					data = JSON.parse(req.res.text);
+
+					assert('message' in data, 'message is not present in Data');
+					done();
+				});
+		});
 	});
+
+	describe('PUT /propositions/:id/take', function () {
+		it('Glenn should take proposition', function (done) {
+
+			var id = new ObjectId(prodsition1ID);
+
+			request(app)
+				.put('api/propositions/' + id + '/take')
+				.set('X-Authorization-Token', glennToken)
+				.expect(200).end(function (err, req) {
+					//data = req.res.body;
+
+					//assert('message' in data, 'message is not present in Data');
+
+					//console.log(data.message);
+					done();
+				});
+		});
+	});
+
+
 
 	after(function (done) {
+		console.log('after');
 
-		User.findByIdAndRemove(zifID, done);
-		//User.findByIdAndRemove(fizID, done);
+		/*Proposition.findByIdAndRemove(prodsition1ID, function (err, results) {
+			console.log('prodsition1ID', results.id);
+		});
+
+		Proposition.findByIdAndRemove(prodsition2ID, function (err, results) {
+			console.log('prodsition2ID', results.id);
+		});
+
+		User.findByIdAndRemove(glennID, function (err, results) {
+			console.log('glennID', results.id);
+		});
+
+		User.findByIdAndRemove(leoID, function (err, results) {
+			console.log('leoID', results.id);
+		});
+		User.findByIdAndRemove(sylID, function (err, results) {
+			console.log('sylID', results.id);
+		});
+
+		User.findByIdAndRemove(oofId, done);*/
+		done();
+
 	});
 });
-
-
-describe('POST /propositions', function () {});
